0e6ce8fa5c03949f741c5a4d06eb009ebc361561
francivaldo4334 <francivaldodev@gmail.com>
2024-04-03 16:26:03 -0300

    feat

--- app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
+++ app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
@@ -4,17 +4,19 @@ import android.inputmethodservice.InputMethodService
 import android.view.KeyEvent
 import android.view.View
 
-class KeyboardService (): InputMethodService(), View.OnKeyListener{
-    private lateinit var keyId:MutableList<Boolean>
+class KeyboardService() : InputMethodService(), View.OnKeyListener {
+    private lateinit var keyId: MutableList<Boolean>
     private var initClick = false
     private var endClick = false
     override fun onCreate() {
         super.onCreate()
     }
-    fun MutableList<Boolean>.clear(){
+
+    fun MutableList<Boolean>.clear() {
     }
+
     override fun onCreateInputView(): View {
-        return layoutInflater.inflate(R.layout.keyboard_layout,null).apply {
+        return layoutInflater.inflate(R.layout.keyboard_layout, null).apply {
             this.setOnClickListener {
                 //caracteres antes
 //                currentInputConnection.getTextBeforeCursor()
@@ -30,19 +32,23 @@ class KeyboardService (): InputMethodService(), View.OnKeyListener{
 
     override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
         keyId.clear()
-        when(v?.id){
-            R.id.btn0 ->{
+        when (v?.id) {
+            R.id.btn0 -> {
                 keyId[0] = true
             }
-            R.id.btn1 ->{
+
+            R.id.btn1 -> {
                 keyId[1] = true
             }
-            R.id.btn2 ->{
+
+            R.id.btn2 -> {
                 keyId[2] = true
             }
-            R.id.btn3 ->{
+
+            R.id.btn3 -> {
                 keyId[3] = true
             }
         }
+        return true
     }
 }
