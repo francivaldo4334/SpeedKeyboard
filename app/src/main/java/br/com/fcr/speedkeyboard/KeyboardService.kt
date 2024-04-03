199d83f24c76f71605a70785058fc46eff814f05
francivaldo4334 <francivaldodev@gmail.com>
2024-04-03 16:41:55 -0300

    feat

--- app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
+++ app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
@@ -1,19 +1,29 @@
 package br.com.fcr.speedkeyboard
 
+import android.annotation.SuppressLint
 import android.inputmethodservice.InputMethodService
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
 
 class KeyboardService() : InputMethodService(), View.OnTouchListener {
-    override fun onCreate() {
-        super.onCreate()
-    }
+    private lateinit var btn0:Button
+    private lateinit var btn1:Button
+    private lateinit var btn2:Button
+    private lateinit var btn3:Button
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateInputView(): View {
         return layoutInflater.inflate(R.layout.keyboard_layout, null).apply {
-            this.setOnClickListener {
-            }
+            btn0 = findViewById(R.id.btn0)
+            btn1 = findViewById(R.id.btn1)
+            btn2 = findViewById(R.id.btn2)
+            btn3 = findViewById(R.id.btn3)
+            btn0.setOnTouchListener(this@KeyboardService)
+            btn1.setOnTouchListener(this@KeyboardService)
+            btn2.setOnTouchListener(this@KeyboardService)
+            btn3.setOnTouchListener(this@KeyboardService)
         }
     }
 
