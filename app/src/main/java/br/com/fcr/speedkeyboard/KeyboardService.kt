538dd92de0020b223b84d56b859b68b2738bd5de
francivaldo4334 <francivaldodev@gmail.com>
2024-04-03 16:37:26 -0300

    feat

--- app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
+++ app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
@@ -2,53 +2,27 @@ package br.com.fcr.speedkeyboard
 
 import android.inputmethodservice.InputMethodService
 import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
 
-class KeyboardService() : InputMethodService(), View.OnKeyListener {
-    private lateinit var keyId: MutableList<Boolean>
-    private var initClick = false
-    private var endClick = false
+class KeyboardService() : InputMethodService(), View.OnTouchListener {
     override fun onCreate() {
         super.onCreate()
-    }
-
-    fun MutableList<Boolean>.clear() {
     }
 
     override fun onCreateInputView(): View {
         return layoutInflater.inflate(R.layout.keyboard_layout, null).apply {
             this.setOnClickListener {
-                //caracteres antes
-//                currentInputConnection.getTextBeforeCursor()
-                //caracteres apos
-//                currentInputConnection.getTextAfterCursor()
-                //deletar caracteres especificos
-//                currentInputConnection.deleteSurroundingText()
-                //adiciona um texto
-//                currentInputConnection.commitText()
             }
         }
     }
 
-    override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
-        keyId.clear()
-        when (v?.id) {
-            R.id.btn0 -> {
-                keyId[0] = true
-            }
-
-            R.id.btn1 -> {
-                keyId[1] = true
-            }
-
-            R.id.btn2 -> {
-                keyId[2] = true
-            }
-
-            R.id.btn3 -> {
-                keyId[3] = true
-            }
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        when (event?.action){
+            MotionEvent.ACTION_DOWN -> {}
+            MotionEvent.ACTION_UP -> {}
+            MotionEvent.ACTION_SCROLL -> {}
         }
-        return true
+       return true
     }
 }
