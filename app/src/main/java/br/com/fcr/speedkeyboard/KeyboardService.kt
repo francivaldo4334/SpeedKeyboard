c5a537a2c3f21bdb4257e27ff256ff3938932fb6
francivaldo4334 <francivaldodev@gmail.com>
2024-04-06 03:47:01 -0300

    j

--- app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
+++ app/src/main/java/br/com/fcr/speedkeyboard/KeyboardService.kt
@@ -3,26 +3,59 @@ package br.com.fcr.speedkeyboard
 import android.annotation.SuppressLint
 import android.inputmethodservice.InputMethodService
 import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
-import androidx.versionedparcelable.ParcelImpl
+import android.widget.TextView
 import br.com.fcr.speedkeyboard.utils.getIdString
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class KeyboardService() : InputMethodService(), View.OnTouchListener {
     private lateinit var buttons: List<Button>
     private val delayTouchTime = 500
     private lateinit var listKeyLastTouchTime: MutableMap<Int, Pair<Boolean,Long>>
     private var resutlIdString = ""
+    private lateinit var shiftIndicator: TextView
+    private var isCapslock = false
+    private var isShift = false
+    private val keymaps = buildMap<String,String> {
+        set("100000","a")
+        set("010000","e")
+        set("001000","i")
+        set("000100","s")
+        set("000010","r")
+        set("000001","n")
+        set("110000","o")
+        set("011000","u")
+        set("000110","h")
+        set("000011","k")
+        set("100100","m")
+        set("010010","d")
+        set("001001","t")
+        set("100010","c")
+        set("010001","l")
+        set("110100","b")
+        set("011010","g")
+        set("101001","f")
+        set("000101","j")
+        set("111000","p")
+        set("001101","w")
+        set("011100","y")
+        set("100011","q")
+        set("110001","x")
+        set("101010","z")
+        set("011001","v")
+        set("000111"," ")
+        set("100001","SHIFT")
+        set("001100","DELETE")
+    }
 
     @OptIn(DelicateCoroutinesApi::class)
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreateInputView(): View {
         return layoutInflater.inflate(R.layout.keyboard_layout, null).apply {
+            shiftIndicator = findViewById(R.id.shift_indicator)
             buttons = buildList {
                 add(findViewById(R.id.btn0))
                 add(findViewById(R.id.btn1))
@@ -78,9 +111,35 @@ class KeyboardService() : InputMethodService(), View.OnTouchListener {
         }
         if (buttons.none { it.isPressed }) {
             currentInputConnection.apply {
-                commitText(resutlIdString + "|", 1)
+                if(keymaps.containsKey(resutlIdString)){
+                    var command = keymaps[resutlIdString]
+                    when(command) {
+                        "DELETE" -> {
+                            deleteSurroundingText(1,0)
+                        }
+                        "SHIFT" -> {
+                            if (isCapslock){
+                                isCapslock = false
+                            }
+                            else if (isShift){
+                                isCapslock = true
+                            }
+                            else {
+                                isShift = true
+                            }
+                        }
+                        else -> {
+                            if (isShift || isCapslock){
+                                command = command?.uppercase()
+                                isShift = false
+                            }
+                            commitText(command, 1)
+                        }
+                    }
+                }
             }
         }
+        Log.d("TS: ", "OK")
         return true
     }
 }
